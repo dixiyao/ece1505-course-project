9ccf91fdcd48b6ca99070c52c23c8fd1082193c5
Dixi Yao <pspo-qfl@163.com>
2022-11-21 16:23:15 -0500

    checked calculation of alphabeta

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ece1551-course-project
 Optimization over Client Selection in Efficient Federated Learning
+
+```math
+\frac{\alpha}{\beta}=-0.0002610568374611641
+```
---
+++ script/calculate_alphabeta.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+p = np.array(pd.read_csv("./results/fedavg_p.csv")["p"])
+G = np.array(pd.read_csv("./results/fedavg_G.csv")["G"])
+
+sump2G2 = np.sum(p * p * G * G)
+sumpG2 = np.sum(p * G * G)
+print(sump2G2, sumpG2)
+
+R1 = 23
+R2 = 22
+
+result = (R1 / R2 - 1) / (100 * sump2G2 - R1 / R2 * sumpG2)
+print(result)
