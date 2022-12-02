5c86026c675b2c8148e2016621170458acd824bf
daxixi <pspo-qfl@163.com>
2022-12-02 15:20:36 -0500

    added 200 p= p=

--- script/calculate_alphabeta.py
+++ script/calculate_alphabeta.py
@@ -1,15 +1,15 @@
 import numpy as np
 import pandas as pd
 
-p = np.array(pd.read_csv("./results/fedavg_p.csv")["p"])
-G = np.array(pd.read_csv("./results/fedavg_G.csv")["G"])
+p = np.array(pd.read_csv("./results/fedavg_200_p.csv")["p"])
+G = np.array(pd.read_csv("./results/fedavg_200_G.csv")["G"])
 
 sump2G2 = np.sum(p * p * G * G)
 sumpG2 = np.sum(p * G * G)
 print(sump2G2, sumpG2)
 
-R1 = 210
-R2 = 134
+R1 = 168
+R2 = 102
 
 result = (R1 / R2 - 1) / (100 * sump2G2 - R1 / R2 * sumpG2)
 print(result)
