b7703f9725856878c0281d26b42db347c265fb78
Dixi Yao <pspo-qfl@163.com>
2022-12-03 10:08:12 -0500

    uploaded results

--- script/draw_train.py
+++ script/draw_train.py
@@ -4,6 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 sns.set_theme(style="whitegrid")
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["font.serif"] = "Times New Roman"
 
 fedavg = pd.read_csv("./results/fedavg_acc_total.csv")
 pi = pd.read_csv("./results/pi.csv")
@@ -11,9 +13,15 @@ convex = pd.read_csv("./results/convex.csv")
 fedavg_ = pd.read_csv("./results/fedavg_client_info.csv")
 pi_ = pd.read_csv("./results/pi_accuracy.csv")
 convex_ = pd.read_csv("./results/convex_accuracy.csv")
+fedavg200 = pd.read_csv("./results/200.csv")
+pi200 = pd.read_csv("./results/200pq.csv")
+convex200 = pd.read_csv("./results/convex200.csv")
+fedavg200_ = pd.read_csv("./results/200_accuracy.csv")
+pi200_ = pd.read_csv("./results/200pq_accuracy.csv")
+convex200_ = pd.read_csv("./results/convex200_accuracy.csv")
 
 times = []
-for method in [fedavg_, pi_, convex_]:
+for method in [fedavg_, pi_, convex_, fedavg200_, pi200_, convex200_]:
     time_ = [0]
     for round_idx in range(min(method["round"]), max(method["round"]) + 1):
         rts = []
@@ -23,13 +31,54 @@ for method in [fedavg_, pi_, convex_]:
         time_.append(time_[-1] + max(rts))
     times.append(time_)
 
-print(times[0][-1], times[1][-1], times[2][-1])
+print(
+    times[0][-1], times[1][-1], times[2][-1], times[3][-1], times[4][-1], times[5][-1]
+)
 
-plt.plot(times[0], [0] + np.array(fedavg["accuracy"]).tolist(), label="FedAvg")
-plt.plot(times[1], [0] + np.array(pi["accuracy"]).tolist(), label="p=q")
-plt.plot(times[2], [0] + np.array(convex["accuracy"]).tolist(), label="Ours")
+# plt.plot(
+#     times[0],
+#     [0] + np.array(fedavg["accuracy"]).tolist(),
+#     label="FedAvg Client 100",
+#     linewidth=1.5,
+# )
+# plt.plot(
+#     times[1],
+#     [0] + np.array(pi["accuracy"]).tolist(),
+#     label="p=q, Client 100",
+#     linewidth=1.5,
+# )
+# plt.plot(
+#     times[2],
+#     [0] + np.array(convex["accuracy"]).tolist(),
+#     label="Ours, Client 100",
+#     linewidth=1.5,
+# )
+plt.plot(
+    times[3],
+    [0] + np.array(fedavg200["accuracy"]).tolist(),
+    label="FedAvg Client 200",
+    linewidth=1.5,
+)
+plt.plot(
+    times[4],
+    [0] + np.array(pi200["accuracy"]).tolist(),
+    label="p=q, Client 200",
+    linewidth=1.5,
+)
+plt.plot(
+    times[5],
+    [0] + np.array(convex200["accuracy"]).tolist(),
+    label="Ours, Client 200",
+    linewidth=1.5,
+)
 
-plt.ylabel("Accuracy")
-plt.xlabel("Wall Clock Time (s)")
-plt.legend()
-plt.savefig("./training.png")
+font = {
+    "family": "Times New Roman",
+    "weight": "bold",
+    "size": 16,
+}
+
+plt.ylabel("Accuracy", fontdict=font)
+plt.xlabel("Wall Clock Time (s)", fontdict=font)
+plt.legend(prop={"weight": "bold", "size": 16, "family": "Times New Roman"})
+plt.savefig("./training_200.png")
